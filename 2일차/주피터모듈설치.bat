da4b7be2ddce2a2c0d2f5dc54a2b31ad584b669d
mjkim912 <sinceree912@gmail.com>
2023-07-21 17:26:32 +0900

    Day2

---
+++ 2일차/주피터모듈설치.bat
@@ -0,0 +1,16 @@
+@echo off
+
+python -m pip install --upgrade pip
+
+pip install jupyter
+pip install pandas
+pip install openpyxl
+pip install xlrd
+pip install xlwt
+pip install matplotlib
+pip install seaborn
+pip install folium
+pip install PIL
+pip install selenium
+
+
